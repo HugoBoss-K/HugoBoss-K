2f7d7c0fa5166db4c03eab2d9273012b9fab53d1
HugoBoss-K <62519513+HugoBoss-K@users.noreply.github.com>
2020-03-22 23:05:53 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo off
+title installation . . .
+npm install
---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+color a
+title Bot Nitro Generateur - By HugoBoss
+:main
+echo Starting ...
+node index
+echo Le bot à crash 
+pause
+cls
+goto main
